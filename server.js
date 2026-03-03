96af1a6203fa1eeeff7d55e2389680c6b28747bf
Naresh Golla <mrgollanaresh95@gmail.com>
2026-03-03 11:57:23 +0530

    Added root route

--- server.js
+++ server.js
@@ -36,3 +36,7 @@ const PORT= process.env.PORT||5000
 app.listen(PORT, () => {
   console.log("Server running on port 5000");
 });
+
+app.get("/", (req, res) => {
+  res.send("Citisolve Backend is Running 🚀");
+});
