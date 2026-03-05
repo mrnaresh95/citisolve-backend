2a6782bd5e6854e8a49016fafba20c43fffb2b8c
Naresh Golla <mrgollanaresh95@gmail.com>
2026-03-05 17:57:23 +0530

    this is backend

--- server.js
+++ server.js
@@ -32,7 +32,8 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-const PORT= process.env.PORT||5000
+const PORT= process.env.PORT || 5000
+
 app.listen(PORT, () => {
   console.log("Server running on port 5000");
 });
